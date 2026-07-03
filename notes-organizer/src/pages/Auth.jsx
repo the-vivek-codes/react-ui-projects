46fbd3229a33eb7c4cac958782a99c0f63f207ab
Vivek Kumar Dwivedi <vivekdubey159999@gmail.com>
2026-07-03 16:46:45 +0530

    Updated Auth page color theme

--- notes-organizer/src/pages/Auth.jsx
+++ notes-organizer/src/pages/Auth.jsx
@@ -37,15 +37,15 @@ export default function Auth() {
         }
     }
     return (
-        <div className='min-h-screen bg-linear-to-br from-cyan-950 via-slate-950 to-black flex items-center justify-center px-6'>
-            <div className='grid lg:grid-cols-2 w-full max-w-5xl bg-slate-900/70 border border-slate-800 rounded-3xl overflow-hidden shadow-2xl'>
-                <div className='hidden lg:flex flex-col justify-center p-16 border-r border-slate-800'>
-                    <h1 className='text-6xl font-bold leading-tight text-white'>
+        <div className='min-h-screen bg-linear-to-br from-zinc-500 via-zinc-200 to-zinc-50 flex items-center justify-center px-6'>
+            <div className='grid lg:grid-cols-2 w-full max-w-5xl bg-white rounded-3xl overflow-hidden shadow-2xl'>
+                <div className="hidden lg:flex flex-col justify-center p-16 border-r border-gray-200">
+                    <h1 className='text-6xl font-bold leading-tight text-gray-900'>
                         Organize Your
-                        <span className='text-cyan-400'> Thoughts</span>
+                        <span className='text-gray-500'> Thoughts</span>
                     </h1>
 
-                    <p className='text-slate-400 mt-6 text-lg'>
+                    <p className='text-gray-600 mt-6 text-lg'>
                         A premium minimalist notes organizer built using React and TailwindCSS
                     </p>
                 </div>
@@ -55,10 +55,10 @@ export default function Auth() {
                     animate={{ opacity: 1, y: 0 }}
                     className='p-10 lg:p-16 flex flex-col justify-center'
                 >
-                    <h2 className='text-4xl font-bold mb-2 text-white'>
+                    <h2 className='text-4xl font-bold mb-2 text-gray-900'>
                         {isLogin ? 'Welcome Back' : 'Create Account'}
                     </h2>
-                     <p className='text-slate-400 mb-10'>
+                     <p className='text-gray-500 mb-10'>
                         {isLogin
                             ? 'Sign in to continue'
                             : 'Register to start organizing'}
@@ -68,25 +68,25 @@ export default function Auth() {
                         placeholder='Email'
                         value={email}
                         onChange={e => setEmail(e.target.value)}
-                        className='bg-slate-950 border border-slate-700 text-white rounded-xl p-4 mb-4 outline-none focus:border-cyan-500 transition'
+                        className="bg-gray-100 border border-gray-300 rounded-xl p-4 mb-4 text-gray-900 placeholder-gray-500 outline-none transition focus:border-black"
                     />
                     <input
                         type='password'
                         placeholder='Password'
                         value={password}
                         onChange={e => setPassword(e.target.value)}
-                        className='bg-slate-950 border border-slate-700 text-white rounded-xl p-4 mb-6 outline-none focus:border-cyan-500 transition'
+                        className="bg-gray-100 border border-gray-300 rounded-xl p-4 mb-4 text-gray-900 placeholder-gray-500 outline-none transition focus:border-black"
                     />
                     <button
                         onClick={handleSubmit}
-                        className='bg-cyan-600 hover:bg-cyan-500 p-4 rounded-xl font-semibold transition'
+                        className="bg-black hover:bg-gray-800 text-white rounded-xl p-4 font-semibold cursor-pointer active:scale-95 transition duration-150"
                     >
                         {isLogin ? 'Sign In' : 'Register'}
                     </button>
-                    <p className='text-slate-400 mt-6'>
+                    <p className='text-gray-600 mt-6'>
                         {isLogin ? 'New here?' : 'Already have an account?'}
-                        <button onClick={() => setIsLogin(!isLogin)} className='ml-2 text-cyan-400'>
-                            {isLogin ? 'Register' : 'Sign In'}
+                        <button onClick={() => setIsLogin(!isLogin)} className='ml-2 text-gray-600 font-semibold cursor-pointer hover:text-black active:scale-95 transition duration-150 inline-block'>
+                            {isLogin ? 'REGISTER' : 'SIGN IN'}
                         </button>
                     </p>
                 </motion.div>
